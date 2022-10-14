b73e1a071257aaa60ac832f127979c62d78d49e0
CeferovaSona <87385383+CeferovaSona@users.noreply.github.com>
2022-10-15 00:58:40 +0400

    add Exercise folder

---
+++ ConsoleApp1/ConsoleApp1/Account.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    //class Account
+    //{
+    //    public static void Login(string username, string password)
+    //    {
+    //        if (username == "cavid123" && password == "Cavid1993")
+    //        {
+    //            Console.WriteLine("Girish ugurludur");
+    //            return;
+    //        }
+    //        else
+    //        {
+    //            Console.WriteLine("username ve password sehvdir");
+    //        }
+    //    }
+    //}
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region task1
+            //int num1, num2, result;
+            //Console.Write("enter first number: ");
+            //num1 = int.Parse(Console.ReadLine());
+
+            //Console.Write("enter second number: ");
+            //num2 = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Enter operation");
+            //string opp = Console.ReadLine();
+
+            //switch (opp)
+            //{
+            //    case "+":
+            //        result = num1 + num2;
+            //        Console.WriteLine("result=" + result);
+            //        break;
+
+            //    case "-":
+            //        result = num1 - num2;
+            //        Console.WriteLine("result=" + result);
+            //        break;
+
+            //    case "*":
+            //        result = num1 * num2;
+            //        Console.WriteLine("result=" + result);
+            //        break;
+
+            //    case "/":
+            //        result = num1 / num2;
+            //        Console.WriteLine("result=" + result);
+            //        break;
+
+            //    default:
+            //        Console.WriteLine("Invalid Operation");
+            //        break;
+            //}
+            //Console.ReadLine();
+            #endregion
+
+            #region Factorial
+            //int num = 5;
+            //int fact = 1;
+            //for (int i = 1; i <=num; i++)
+            //{
+            //    fact = fact * i;
+            //}
+            //Console.WriteLine(fact);
+            //Console.ReadLine();
+            
+            
+            #endregion
+
+            //Login("cavid123", "Cavid1993") ;
+
+
+        }
+    }
+}
